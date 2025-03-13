dbbe7ccfa205c61ad41043b0dd4cce070869fead
zm <2679031498@qq.com>
2025-03-13 14:19:26 +0800

    移植rtos

--- boot/start.s
+++ boot/start.s
@@ -50,13 +50,13 @@ _start:
 	load_data a0,a1,a2
 
 	//load trusted_fw.bin
-	//[0x20400000:0x20800000] --> [0xb0000000:0xb0400000]
+	//[0x20400000:0x20800000] --> [0xbf800000:0xbfc00000]
     li		a0,	0x204
 	slli	a0,	a0, 20      //a0 = 0x20400000
-    li		a1,	0xb00
-	slli	a1,	a1, 20      //a1 = 0xb0000000
-    li		a2,	0xb04
-	slli	a2,	a2, 20      //a2 = 0xb0400000
+    li		a1,	0xbf8
+	slli	a1,	a1, 20      //a1 = 0xbf800000
+    li		a2,	0xbfc
+	slli	a2,	a2, 20      //a2 = 0xbfc00000
 	load_data a0,a1,a2
 
 	# //load qemu_uboot.dtb
